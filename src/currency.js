fc437e98b9ce441c63b658312d26a6bf17604092
Kate Kiatsiri <keidsiri@gmail.com>
2021-11-19 17:03:09 -0800

    Refactor codes from promise to fetch

--- src/currency.js
+++ src/currency.js
@@ -1,21 +1,43 @@
-export default class CurrentService {
-  static getCurrency () {
-    return new Promise (function (resolve, reject){
-      let request = new XMLHttpRequest();
-      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
-      request.onload = function () {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject (request.response);
-        }
-      };
-      request.open("GET", url, true);
-      request.send();
-    });
-  }
+// export default class CurrentService {
+//   static getCurrency(code) {
+//     return new Promise (function (resolve, reject){
+//       let request = new XMLHttpRequest();
+//       const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${code}`;
+//       request.onload = function () {
+//         if (this.status === 200) {
+//           resolve(request.response);
+//         } else {
+//           reject (request.response);
+//         }
+//       };
+//       request.open("GET", url, true);
+//       request.send();
+//     });
+//   }
 
-  convert (usd,rate) {
-    return usd/rate;
+//   convert (usd,rate) {
+//     return usd/rate;
+//   }
+// }
+
+export class CurrentService {  
+  static getCurrency(code) {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${code}`)
+      .then(function(response) {
+        if(response.status === 404) {
+          throw Error(response.statusText);
+        } else if(!response.ok){
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function(error) {
+        return Error(error);
+      });
   }
+}
+
+
+export function convert(usd, rate){
+  return usd/rate;
 }
--- src/index.js
+++ src/index.js
@@ -2,24 +2,46 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import CurrentService from './currency.js';
+// import CurrentService from './currency.js';
+import {CurrentService, convert} from './currency.js';
+
+
+// function displayErrors(error) {
+//   $('.showErrors').text(`There is${error}`);
+// }
 
 $(document).ready(function() {
-  $('#currencyConvert').val("");
+  // $('#currencyConvert').val("");
 
-  let promise = CurrentService.getCurrency();
-  promise.then(function(response) {
-    const body = JSON.parse(response);
-    Object.keys(body.conversion_rates).forEach(key => {
-      let value = body.conversion_rates[key];
-      $('.showAllRates').append(`${key}: ${value} <br>`);
-      console.log(`key ${key}: value ${value}`);
-    });
-  }, function(error) {
-    $('.showErrors').text(`There was an error processing your request: ${error}`);
+  // let promise = CurrentService.getCurrency();
+  // promise.then(function(response) {
+  //   const body = JSON.parse(response);
+  //   Object.keys(body.conversion_rates).forEach(key => {
+  //     let value = body.conversion_rates[key];
+  //     $('.showAllRates').append(`${key}: ${value} <br>`);
+  //     console.log(`key ${key}: value ${value}`);
+  //   });
+  // }, function(error) {
+  //   $('.showErrors').text(`There was an error processing your request: ${error}`);
+  // });
+
+  $('#convert').click(function() {
+    let code = $("#code").val();
+    // $('#convert').val("");
+    CurrentService.getCurrency(code)
+      .then(function(response) {
+        const body = response;
+        let rate = body.conversion_rates.USD;
+        let usd = $('#number').val();
+        let exchange = convert(usd, rate);
+        $('#showRate').html(exchange);
+      })
+      .catch(function(error) {
+        $('.showErrors').html(`There is an error please try your currency code again : ${error}`);
+        $('ShowErrors').val("")
+      });
+
   });
-
-  
 });
 
 
